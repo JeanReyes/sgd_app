9257123795c4c2983e1cff0dcb9504f4c9378635
Jean R <jereyesal@falabella.cl>
2024-07-02 11:35:33 -0400

    chore: Update next.config.js and package-lock.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,40 @@
-import { Inter } from '@next/font/google'
-import { Layout } from '../components/layouts'
-import { useContext } from 'react'
-import { AuthContext } from '../context/Auth/AuthContext';
-import { Button, Card, CardContent, FormControl, FormLabel, Grid, Paper, Typography } from '@mui/material';
+import { Inter } from "next/font/google";
+import { Layout } from "../components/layouts";
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth/AuthContext";
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  FormLabel,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // defimos el layout de la app
+  return (
+    <Layout>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>
+              <Typography variant="h3">Bienvenido a SGD aaa</Typography>
+            </FormLabel>
+            <Grid>
+              <Typography>Elija una opción:</Typography>
 
-    // defimos el layout de la app
-    return (
-        <Layout> 
-              <Card>
-                <CardContent>
-                    <FormControl>
-                        <FormLabel>
-                            <Typography variant='h3'>Bienvenido a SGD </Typography>
-                            hola mundo
-                        </FormLabel>
-                        <Grid>
-                            <Typography> 
-                                Elija una opción:
-                            </Typography>
-
-                            <Button variant="contained" >Contained</Button>
-                            <Button variant="text">Crear documento</Button>
-                            <Button variant="outlined">Outlined</Button>    
-                        </Grid>
-                    </FormControl>
-                </CardContent>
-            </Card>
-        </Layout>
-    )
+              <Button variant="contained">Contained</Button>
+              <Button variant="text">Crear documento</Button>
+              <Button variant="outlined">Outlined</Button>
+            </Grid>
+          </FormControl>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
 }
